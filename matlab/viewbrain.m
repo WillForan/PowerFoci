6f4435cd795af14d8bf502a32d96d4d8e4fc9c2b
Will Foran <foranw@upmc.edu>
2012-03-23 17:16:27 -0400

    recenter,rescale, and resample template brain mesh

--- matlab/viewbrain.m
+++ matlab/viewbrain.m
@@ -1,29 +1,61 @@
 % display a brain!
 
-img=load_nii('MNI152lin_T1_2mm_brain_mask.nii');
+%load_nii from http://www.mathworks.com/matlabcentral/fileexchange/8797
+img=load_nii('MNI152_T1_2mm_brain_mask.nii');
 brain=double(img.img);
+
+%code from http://soundsfromthenorth.com/?p=3
+%img.img is 91x109x91
+[xsize,ysize,zsize]=size(img.img);
 prev=0;
 p=[];
-for x=1:91;
-   for y=1:109
-      for z=1:91;
+
+% find edges
+% v is 0 or 1
+% only care about edges (changes in v<->prev) 
+for x=1:xsize;
+   for y=1:ysize;
+      for z=1:zsize;
          v=brain(x,y,z);
-         if(prev~=v)
-            p=[p; x y z];
-         end
+         
+         if(                                  ...
+             prev~=v &&                       ... % there must be a change
+              (                               ...
+                isempty(p) ||                 ... % always take the first
+                pdist([x y z;p(end,:)]) > 15  ... % skip if too close
+              )                               ...
+            )
+            p = [p; x y z];
             prev=v;
+         end
+
       end
    end
 end
 
+
+% downsample -- take 5x fewer points
+% use pdist computation in for loop instead
+% p=p(1:5:end,:);
+
+% brain is 2mm so multiply positions by 2
+p = p .* 2;
+
+% and then recenter
+%  assume dimensions are symetric
+%  and the mean should be zero
+for dim=1:3
+    p(:,dim) = p(:,dim)  - mean( p(:,dim) );
+end
+
+
+
+%MyRobustCrust from http://code.google.com/p/atlasnavigator/
 [t,tnorm]=MyRobustCrust(p);
-figure(1)
-hold on
-axis equal
 h=trisurf( t,p(:,1),p(:,2),p(:,3),   ...
            'facealpha',0.4,          ...
-           'facecolor',[1 1 1],      ...
-           'edgecolor',[0.8 0.8 0.8] ...
+           'facecolor',[.7 .7 .7],   ...
+           'edgecolor',[.8 .8 .8]    ...
          );
-view(3);
-axis vis3d
+% view(3);
+% axis vis3d
