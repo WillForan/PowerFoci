7c39d3018ac25d0128cd8686a62ad3ac388cae34
Will Foran <foranw@upmc.edu>
2012-04-20 12:07:15 -0400

    create whole brain ROIs with 3dundump for corrected bpvsSim comm

---
+++ withAFNI/createSpheres_bp_sim.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+for type in {5,6}; do
+   case $type in
+      5) name=bpreg  ;;
+      6) name=simult ;;
+   esac
+
+   3dUndump  -xyz -srad 7 -overwrite                          \
+             -prefix left_$name.nii.gz                        \
+             -master mni_icbm152_t1_tal_nlin_asym_09c_2mm.nii \
+             <(awk -F,  '{print $1,$2,$3,$'$type'}'           \
+                        ../matrix/bb244_communities_bpreg_simult-corrected.csv)
+           
+done
